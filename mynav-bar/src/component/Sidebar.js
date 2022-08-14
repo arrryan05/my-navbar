8075faf537e1375764927f20c513725074a23289
Aryan Mishra <mishra2001aryan@gmail.com>
2022-08-14 23:57:02 +0530

    final touches

--- mynav-bar/src/component/Sidebar.js
+++ mynav-bar/src/component/Sidebar.js
@@ -11,6 +11,7 @@ function Sidebar() {
             <li
               key={key}
               className="row"
+              id={window.location.pathname == val.link ? "active": ""}
               onClick={() => {
                 window.location.pathname = val.link;
               }}
